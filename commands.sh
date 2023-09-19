5ebd3e93681949ff7db935d2d28c258b671b1833
Cong Dinh <congdinh2012@hotmail.com>
2023-09-19 16:33:42 +0700

    Add terraform to create resource

--- commands.sh
+++ commands.sh
@@ -5,11 +5,12 @@ helm repo add udacity-pr3 https://charts.bitnami.com/bitnami
 helm install udacity-postgre udacity-pr3/postgresql
 
 # The password can be retrieved with the following command:
-export POSTGRES_PASSWORD=$(kubectl get secret --namespace default udacity-postgre-postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
+export POSTGRES_PASSWORD=$(kubectl get secret --namespace default postgres-postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
 echo $POSTGRES_PASSWORD
 
 # Connecting Via Port Forwarding
-kubectl port-forward --namespace default svc/udacity-postgre-postgresql 5432:5432 & PGPASSWORD=jVQXWgZXj6 psql --host 127.0.0.1 -U postgres -d postgres -p 5432
+kubectl port-forward --namespace default svc/postgres-postgresql 5432:5432 & 
+PGPASSWORD=ZWhUiLHn1a psql --host 127.0.0.1 -U postgres -d postgres -p 5432
 
 
 # Connecting Via a Pod
@@ -20,6 +21,6 @@ PGPASSWORD="jVQXWgZXj6" psql postgres://postgres@udacity-postgre-postgresql:5432
 kubectl port-forward svc/udacity-postgre-postgresql 5432:5432
 
 kubectl port-forward --namespace default svc/udacity-postgre-postgresql 5432:5432 &
-    PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < ../db/1_create_tables.sql
+    PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < ./db/3_seed_tokens.sql
 
 DB_USERNAME=postgres DB_PASSWORD=jVQXWgZXj6 python app.py
