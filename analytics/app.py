eb26addd562ed72efbf30e853642335002d348fe
Cong Dinh <congdinh2012@hotmail.com>
2023-09-20 00:16:23 +0700

    Update analytics app health and readiness check

--- analytics/app.py
+++ analytics/app.py
@@ -8,10 +8,37 @@ from sqlalchemy import and_, text
 from random import randint
 
 from config import app, db
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 
 
 port_number = int(os.environ.get("APP_PORT", 5153))
 
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    joined_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.id
+
+class Token(db.Model):
+    __tablename__ = "tokens"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    token = Column(String(6), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    used_at = Column(DateTime)
+
+    def __repr__(self):
+        return "<Token %r>" % self.id
+    
+with app.app_context():
+    db.create_all()
 
 @app.route("/health_check")
 def health_check():
